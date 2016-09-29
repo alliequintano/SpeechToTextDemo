268f189d045bba682e9f80699796fae015b24400
Allie Q <aquintano22@gmail.com>
2016-09-29 15:28:38 -0400

    first commit

---
+++ Assets/KeywordScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class KeywordScript : MonoBehaviour
+{
+    [SerializeField]
+    private string[] m_Keywords;
+
+    private KeywordRecognizer m_Recognizer;
+    public GameObject cube;
+    public GameObject sphere;
+
+    void Start()
+    {
+        m_Keywords = new string[2];
+        m_Keywords[0] = "Cube";
+        m_Keywords[1] = "Sphere";
+        m_Recognizer = new KeywordRecognizer(m_Keywords);
+        m_Recognizer.OnPhraseRecognized += OnPhraseRecognized;
+        m_Recognizer.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+
+        float newX = UnityEngine.Random.Range(-10, 10);
+        float newY = UnityEngine.Random.Range(-10, 10);
+        float newZ = UnityEngine.Random.Range(5, 10);
+
+       if (args.text == m_Keywords[0])
+        {
+            Instantiate(cube, new Vector3(newX, newY, newZ), Quaternion.identity);
+        }
+        if (args.text == m_Keywords[1])
+        {
+            Instantiate(sphere, new Vector3(newX, newY, newZ), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/VoiceRecognition.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class VoiceRecognition : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_Hypotheses;
+
+    [SerializeField]
+    private Text m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+
+    void Start()
+    {
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            m_Recognitions.text = text;
+
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+            m_Hypotheses.text += text;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            //if (cause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.");
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+
+        m_DictationRecognizer.Start();
+    }
+}
